6a35cca11a31a824194c77dcab520e3da305dad7
Diogo <daraujo.augusto@gmail.com>
2019-09-22 17:23:44 -0400

    Starting App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'heros-app';
+  title = 'Meu Primeiro Projeto Angular';
 }
